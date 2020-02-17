17500de3544cf645f343cf6a3f5a8b3b15074bff
HJSONyoy <36266065+HJSONyoy@users.noreply.github.com>
2020-02-17 20:08:46 +0900

    Update index.js

--- WebImageCropper/routes/index.js
+++ WebImageCropper/routes/index.js
@@ -1,21 +1,39 @@
 var express = require('express');
+const request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
-/* MY SAMPLE PAGE */
-// router.get('/image-crop-with-nolib', function(req, res, next) {
-//   res.render('image-crop', { title: 'Express' });
+// router.get('/cropPage', function(req, res, next) {
+//     res.render('cropPage', { title: 'Express' });
 // });
 
 /* CANVAS LIBRARY PAGE */
 router.get('/image-crop-with-cropper', function(req, res, next) {
-  res.render('img-canvas', { title: 'Express' });
+
+    //FOR SPECIFIC SERVER PARSING
+    // let imageList = [];
+    // let url = "http://13.67.47.175:8880/pokemon/downloads/Pikachu/";
+    // request.get({
+    //     uri: url
+    // }, function(error, response, body) {
+    //     let lists = response.body.split('<a href="').slice(2, -1);
+    //     lists.forEach(function(item) {
+    //         imageList.push(url + item.split('"')[0]);
+    //     });
+    //     res.render('img-canvas', {
+    //         title: 'img-canvas',
+    //         imageList: imageList
+    //     });
+
+    // });
+
+    res.render('img-canvas', {
+        title: 'img-canvas',
+    });
+
 });
-
-
-
 module.exports = router;
